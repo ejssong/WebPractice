2b1c47b3a27dcd821dfb5bce2871c9abc3323c83
Eunjin Song <eunjinsong8376@gmail.com>
2020-08-20 17:19:03 +0900

    create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="styles.css" rel="stylesheet" type="text/css">
+
+<header>
+    <div class="nav-logo">
+            <a href="index.html">Eunjin Song </a>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#resume">Résumé</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>   
+    </nav>
+</header>
